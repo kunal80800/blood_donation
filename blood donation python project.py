cacd8385b2beb48a5839f1579f7cffe77b7d18b6
kunal <kunaljadhav80800@gmail.com>
2023-08-02 23:07:22 +0530

    initial commit

---
+++ blood donation python project.py
@@ -0,0 +1,12 @@
+--Blood Donation--
+ 
+age = int(input('enter your age :'))
+if age>=18:
+    print('okay')
+    weight = int(input('enter your weight :'))
+    if weight>= 50:
+        print('success')
+    else:
+        print('end')
+else:
+    print('invalid')
